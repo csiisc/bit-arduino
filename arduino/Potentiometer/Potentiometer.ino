f723843a91e76837b5c58c031e747f51fd001bcf
bit <bit@bit-PC>
2014-12-13 17:52:42 +0900

    day2

---
+++ arduino/Potentiometer/Potentiometer.ino
@@ -0,0 +1,36 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(3, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(0);
+  Serial.println(val);
+  
+  if(val <= 255) {
+    digitalWrite(3, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(9, LOW);
+  }
+  else if(val > 255 && val <= 511) {
+    digitalWrite(3, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(9, LOW);
+  }
+  else if(val > 511 && val <= 767) {
+    digitalWrite(3, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(9, LOW);
+  }
+  else {
+    digitalWrite(3, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(9, HIGH);
+  }
+  
+  //val = map(val, 0, 1023, 0, 255);
+}
---
+++ arduino/_7segment/_7segment.ino
@@ -0,0 +1,57 @@
+Servo servoMotor;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for(int i = 0; i < 8; i++) {
+    pinMode(i, OUTPUT);
+  }
+  
+  servoMotor.attach(10);
+  servoMotor.write(0);
+  delay(1000);
+}
+
+static int segment_table [10][8] {
+  {1, 1, 1, 1, 1, 1, 0, 0}, //0
+  {0, 0, 1, 1, 0, 0, 0, 0}, //1
+  {1, 1, 0, 1, 1, 0, 1, 0}, //2
+  {0, 1, 1, 1, 1, 0, 1, 0}, //3
+  {0, 0, 1, 1, 0, 1, 1, 0}, //4
+  {0, 1, 1, 0, 1, 1, 1, 0}, //5
+  {1, 1, 1, 0, 1, 1, 1, 0}, //6
+  {0, 0, 1, 1, 1, 1, 0, 0}, //7
+  {1, 1, 1, 1, 1, 1, 1, 0}, //8
+  {0, 1, 1, 1, 1, 1, 1, 0} //9
+};
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 0; i < 10; i++) {
+    Serial.println(i);
+    for(int j = 0; j < 8; j++) {
+      Serial.print(j);
+      if(segment_table[i][j] == 1) {
+        Serial.println("LOW");
+        digitalWrite(j + 1, LOW);
+      }
+      else {
+        Serial.println("HIGH");
+        digitalWrite(j + 1, HIGH);
+      }
+    }
+    delay(1000);
+  }
+  
+  for(angle = 0; angle <= 179; angle++) {
+    servoMotor.write(angle);
+    delay(10);
+  }
+  delay(100);
+  
+  for(angle = 179; angle >= 0; angle--) {
+    servoMotor.write(angle);
+    delay(10);
+  }
+  delay(100);
+}
